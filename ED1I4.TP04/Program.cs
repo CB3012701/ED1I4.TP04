5c47e78ade7797ffde7395df74c3c7d93bc1b087
CB3012701 <82848078+CB3012701@users.noreply.github.com>
2023-10-09 21:56:43 -0300

    Add files via upload

---
+++ ED1I4.TP04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ED1I4.Atividade4
+{
+    static class Program
+    {
+    /*
+     *PARTICIPANTES
+     * ÉRICA BARBOSA  CB3012701  */
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
